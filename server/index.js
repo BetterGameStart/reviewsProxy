749f260aa7379843124dc4614f5b0cbceb458ba2
root <marcusbarnesdev@gmail.com>
2020-06-03 18:49:39 -0700

    go live

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+require('newrelic');
 
 const app = express();
 const jsonParser = bodyParser.json();
 const PORT = 3121;
-//const REVIEWS = 'http://localhost:3002/';
-const REVIEWS = 'http://3.22.26.129/';
-const CART = 'http://18.221.207.9:3001/';
+const REVIEWS = 'http://ec2-52-72-35-10.compute-1.amazonaws.com:3002/';
+// const REVIEWS = 'http://3.22.26.129/';
+// const CART = 'http://18.221.207.9:3001/';
 //const CART = 'http://localhost:3001/';
-const CAROUSEL = 'http://3.18.37.38:3003/';
+// const CAROUSEL = 'http://3.18.37.38:3003/';
 //const CAROUSEL = 'http://localhost:3003/';
-const DETAILS = 'http://54.69.127.206:3004/';
+// const DETAILS = 'http://54.69.127.206:3004/';
 //const DETAILS = 'http://localhost:3004/';
-const RECOMMENDED = 'http://3.18.37.38:3005/';
+// const RECOMMENDED = 'http://3.18.37.38:3005/';
 //const RECOMMENDED = 'http://localhost:3005/';
 
 app.listen(PORT, () => { console.log(`Now listening on port ${PORT}`); });
@@ -44,82 +45,82 @@ app.all('/reviews/:gameId', jsonParser, (req, res) => {
     });
 });
 
-app.all('/cartapi/:gameId', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
-  axios({
-    method: METHOD,
-    url: `${CART}cartapi/${req.params.gameId}`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
-});
+// app.all('/cartapi/:gameId', jsonParser, (req, res) => {
+//   const METHOD = req.method;
+//   const BODY = req.body;
+//   axios({
+//     method: METHOD,
+//     url: `${CART}cartapi/${req.params.gameId}`,
+//     data: JSON.stringify(BODY),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then((response) => {
+//       res.status(200).send(response.data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).send('Something went wrong!');
+//     });
+// });
 
-app.all('/carousel/:gameId', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
-  axios({
-    method: METHOD,
-    url: `${CAROUSEL}carousel/${req.params.gameId}`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
-});
+// app.all('/carousel/:gameId', jsonParser, (req, res) => {
+//   const METHOD = req.method;
+//   const BODY = req.body;
+//   axios({
+//     method: METHOD,
+//     url: `${CAROUSEL}carousel/${req.params.gameId}`,
+//     data: JSON.stringify(BODY),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then((response) => {
+//       res.status(200).send(response.data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).send('Something went wrong!');
+//     });
+// });
 
-app.all('/recommended/', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
-  axios({
-    method: METHOD,
-    url: `${RECOMMENDED}recommended/`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
-});
+// app.all('/recommended/', jsonParser, (req, res) => {
+//   const METHOD = req.method;
+//   const BODY = req.body;
+//   axios({
+//     method: METHOD,
+//     url: `${RECOMMENDED}recommended/`,
+//     data: JSON.stringify(BODY),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then((response) => {
+//       res.status(200).send(response.data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).send('Something went wrong!');
+//     });
+// });
 
-app.all('/api/games/:gameId', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
-  axios({
-    method: METHOD,
-    url: `${DETAILS}api/games/${req.params.gameId}`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
-});
+// app.all('/api/games/:gameId', jsonParser, (req, res) => {
+//   const METHOD = req.method;
+//   const BODY = req.body;
+//   axios({
+//     method: METHOD,
+//     url: `${DETAILS}api/games/${req.params.gameId}`,
+//     data: JSON.stringify(BODY),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then((response) => {
+//       res.status(200).send(response.data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).send('Something went wrong!');
+//     });
+// });
